bcd5addc61caa3252e6b4a374f640c81b57d626c
xTCry [Vladislav Kh] <wolt6@mail.ru>
2025-12-21 05:42:56 +0300

    feat: add `MaxException` export

--- lib/index.ts
+++ lib/index.ts
@@ -6,4 +6,5 @@ export * from './util';
 export * from './max.constants';
 export * from './max.module';
 export * from './max-core.module';
+export * from './max.exception';
 export * from './max-explorer.service';
---
+++ lib/max.exception.ts
@@ -0,0 +1,1 @@
+export class MaxException extends Error {}
