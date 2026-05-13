11d9abe54f6179e5a7b00f50d83d660ac2768c4d
xTCry [Vladislav Kh] <wolt6@mail.ru>
2026-05-13 19:08:21 +0300

    chore(sample): support local package link copy

--- scripts/copy-to-sample.js
+++ scripts/copy-to-sample.js
@@ -7,10 +7,21 @@ const samples = fs.readdirSync('sample');
 
 for (const sample of samples) {
   const targetDir = path.resolve('sample', sample, 'node_modules', libraryName);
+  const targetRealDir = fs.existsSync(targetDir)
+    ? fs.realpathSync(targetDir)
+    : targetDir;
+  const projectDir = path.resolve('.');
+
+  if (targetRealDir === projectDir) {
+    console.log(`◘ Skipped '${libraryName}' copy to 'sample/${sample}'`);
+    continue;
+  }
 
   fs.mkdirSync(targetDir, { recursive: true });
   fs.cpSync('dist', targetDir + '/dist', { recursive: true, force: true });
   fs.copyFileSync('package.json', path.join(targetDir, 'package.json'));
 
-  console.log(`Copied '${libraryName}' to 'sample/${sample}/node_modules/${libraryName}'`);
+  console.log(
+    `○ Copied '${libraryName}' to 'sample/${sample}/node_modules/${libraryName}'`,
+  );
 }
